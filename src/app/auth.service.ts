93bbeebb685a6094d7f359742263cb06bb99ca9e
MrSaucer <108151584+MrSaucer@users.noreply.github.com>
2023-09-27 20:36:39 -0300

    Add files via upload

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'; //<-
+import { Observable } from 'rxjs'; //<-
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +8,19 @@ import { Observable } from 'rxjs';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  loginUser(username: string, password: string): Observable<any> {
-    return this.http.post('http://localhost:3000/banana', {
-      username,
-      password
+  loginUser(user: string, password: string): Observable<any> {
+    return new Observable(observer => {
+      this.http.get<any[]>('http://localhost:3000/users').subscribe(users => {
+        console.log("Fetched users:", users); // Debug line
+        const username = users.find(u => u.user === user && u.password === password);
+        if (username) {
+          observer.next({ success: true });
+        } else {
+          observer.next({ success: false });
+        }
+        observer.complete();
+      });
     });
   }
 }
+  
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,28 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage {
-  username?: string;
+  user?: string;
   password?: string;
-  
 
-  constructor(private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  redirectToLoader(){
-    if (this.username === 'admin' && this.password === '123') {
-      this.router.navigate(['/loader']);
-      
-      setTimeout(() => {
-        this.router.navigate(['/dashboard']);
-        alert('User successfully logged in.');
-      }, 2000);
+  redirectToLoader() {
+    if (this.user && this.password) {
+      this.authService.loginUser(this.user, this.password).subscribe(data => {
+        if (data.success) {
+          this.router.navigate(['/loader']);
+          setTimeout(() => {
+            this.router.navigate(['/dashboard']);
+            alert('User successfully logged in.');
+          }, 2000);
+        } else {
+          alert('Incorrect username or password.');
+        }
+      });
     } else {
-      alert('Incorrect username or password.');
+      alert('Both fields are required.');
     }
   }
 }
